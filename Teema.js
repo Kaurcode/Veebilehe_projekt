18536b1472ee0711217e4743014757f8f90734a1
Kaurcode <kaurkivi@gmail.com>
2023-11-14 00:04:25 +0200

    Häääästi väiksed muudatused JavaScript failis

--- Teema.js
+++ Teema.js
@@ -4,10 +4,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const teemanupp = document.getElementById('TeemaNupp');
     /*const teemastiil = document.getElementById('teema-style');*/
     const kehaelement = document.body;
-
     const susteemiteema = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-    // Set the initial theme based on system preference
     if (susteemiteema) {
         /*teemastiil.href = 'tume-teema-style.css';*/
         kehaelement.classList.add('tume-teema');
